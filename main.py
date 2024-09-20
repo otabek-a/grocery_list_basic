c52fe93525ad32c626b98ab13e51860303ea1eb2
m1 <uzbzarif@gmail.com>
2024-09-20 17:31:35 +0500

    task

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ def get_prices(data):
     Returns:
         list: list of prices
     """
-    prices = []
-    for row in data.split('\n')[1:]:
-        price = row.split(',')[2]
-        prices.append(float(price[1:]))
-    return prices
+
 
 def get_products(data):
     """
@@ -27,13 +23,7 @@ def get_products(data):
     Returns:
         list: list of products
     """
-    data = data.split('\n')[1:]
-    products = []
-    for row in data:
-        product = row.split(',')[0]
-        products.append(product)
-    return products
-   
+
 def get_expensive(prices):
     """
     Finds the most expensive product of index
@@ -44,23 +34,9 @@ def get_expensive(prices):
     Returns:
         int: index of most expensive product
     """
-    idx = 0 # Most expensive product's index
-    expensive = prices[idx]
-    for i in range(len(prices)):
-        if prices[i] > expensive:
-            expensive = prices[i]
-            idx = i
-    return idx
+
     
 
 # Read data from file
 f = open("data.csv")
 data = f.read()
-products = get_products(data)
-# print(products)
-prices = get_prices(data)
-# Get most expensive product index
-idx = get_expensive(prices)
-print(prices[idx])
-# Print most expensive product
-print(f'Most expensive product: {products[idx]}')
